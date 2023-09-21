29962239de648098ec02a9ccd5cd895339fdabfc
ohgukkwon <ohguk.kwon@gmail.com>
2023-09-20 20:28:25 -0700

    esp32_hello

---
+++ ESP32Hello/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(921600);
+  Serial.println("Hello world_setup");
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN,HIGH);
+  Serial.println("Hello world_loop");
+  delay(1000);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(1000);
+}
