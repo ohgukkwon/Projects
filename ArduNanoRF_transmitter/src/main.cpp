853361055a5e1a3f6df8b3b264dcb360c29789b0
ohgukkwon <ohguk.kwon@gmail.com>
2023-09-25 19:48:38 -0700

    9/25

--- ArduNanoRF_transmitter/src/main.cpp
+++ ArduNanoRF_transmitter/src/main.cpp
@@ -11,7 +11,7 @@
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 DHT dht(DHTPIN, DHTTYPE);
  
-String rData = "#000STA";
+//String rData = "#000STA";
 
 //actual turbo reponse  = "#000,\x85\x8b\xcc,25090,002,002,001,001,000,66,00800,070,037,0000000000000000000000000";
 SoftwareSerial mySerial(10, 11); // RX, TX
@@ -36,18 +36,31 @@ void dht11();
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // Configure LiquidCrystal_I2C library with 0x27 address, 16 columns and 2 rows
 //sda #a4, scl #a5
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
 
 void setup() {
 
   // int bard_rate = 115200;
-  // Serial.begin(115200);
-  // Serial.setTimeout(1000);
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
   // mySerial.begin(115200);
   // mySerial.setTimeout(1000);
   // Serial.print("USB Serial is ready");   // opens serial port, sets data rate to 9600 bps
 
   lcd.init();                            // Initialize I2C LCD module
   lcd.backlight();  
+
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 
 }
 
@@ -69,6 +82,12 @@ void loop() {
   //   lcd.print("                ");
   // }
   dht11();
+
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  Serial.print("\n");
+  Serial.println(text);
+
   delay(1000);
 }
 
@@ -78,7 +97,7 @@ void dht11(){           //DHT11 Temp and Humidity sensor
   float f = dht.readTemperature(true); /*variable to store temperature in Fahrenheit*/
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Failed to read from DHT sensor!");
-    lcd.setCursor(0, 2);               // Go to column 0, row 0
+    lcd.setCursor(0, 0);               // Go to column 0, row 0
     lcd.print("DHT Fail");
     return;
   }
