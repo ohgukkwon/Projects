98d81aadeb65fde2ed4d8c09f4e6ebd4d7dc7352
ohgukkwon <ohguk.kwon@gmail.com>
2023-10-15 06:25:23 -0700

    10/15

--- ArduNanoRF_transmitter/src/main.cpp
+++ ArduNanoRF_transmitter/src/main.cpp
@@ -8,12 +8,20 @@ RF24 radio(7, 8); // CE, CSN
 LiquidCrystal_I2C lcd(0x27, 20, 4);  // Configure LiquidCrystal_I2C library with 0x27 address, 16 columns and 2 rows
 //sda #a4, scl #a5
 
-#include "max6675.h"
-int thermoDO = 4; // SO
-int thermoCS = 5; // CS
-int thermoCLK = 6; // SCK
-// float temp1 = 0;
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+// #include "max6675.h"
+// int thermoDO = 4; // SO
+// int thermoCS = 5; // CS
+// int thermoCLK = 6; // SCK
+// // float temp1 = 0;
+// MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+
+#include "DHT.h"
+#define DHTPIN 4     /*Nano pin 4 for DHT11 sensor input*/
+#define DHTTYPE DHT11   /*DHT sensor type we are using*/
+//#define DHTTYPE DHT22   // DHT 22 (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+DHT dht(DHTPIN, DHTTYPE);
+//void dht11();
 
 const byte address[6] = "00001";
 struct SendData{
@@ -50,7 +58,11 @@ void loop()
     sendData.tx_id= 1;
     sendData.cnt = i;
     //sendData.str_temp= "Arduino";
-    sendData.max6675_c = thermocouple.readCelsius();
+    //sendData.max6675_c = thermocouple.readCelsius();
+
+    sendData.dht_hum = dht.readHumidity(); /*float variable that stores humidity value*/
+    sendData.dht_c = dht.readTemperature(); /*float variable that store temperature in Celsius*/
+    sendData.dht_f = dht.readTemperature(true); /*variable to store temperature in Fahrenheit*/
 
     radio.write(&sendData, sizeof(sendData));
     // Serial.println(text);  
@@ -60,7 +72,7 @@ void loop()
     lcd.print(",Ard, ");
     lcd.print(sendData.cnt);
     lcd.setCursor(0,1);
-    lcd.print(sendData.max6675_c,1);
+    lcd.print(sendData.dht_c,1);
     i++;
   }
   else
