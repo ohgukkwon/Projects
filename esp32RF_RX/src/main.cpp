514e5eb0df675dab384d4955111692a7b06a91e0
ohgukkwon <89101938+ohgukkwon@users.noreply.github.com>
2025-05-11 21:28:43 -0700

    Update main.cpp

--- esp32RF_RX/src/main.cpp
+++ esp32RF_RX/src/main.cpp
@@ -2,10 +2,13 @@
 //#include <nRF24L01.h>
 #include <RF24.h>
 
-
 // Define pins for NRF24L01
 #define CE_PIN 4
 #define CSN_PIN 5
+
+// Define pin for DHT11
+#define DHTPIN 14
+#define DHTTYPE DHT11
 
 // Create instances
 RF24 radio(CE_PIN, CSN_PIN);
@@ -13,45 +16,51 @@ RF24 radio(CE_PIN, CSN_PIN);
 // Define the address through which two modules communicate
 const byte address[6] = "00001";
 
+// Structure to hold sensor data
+struct SensorData {
+  float temperature;
+  float humidity;
+};
 
 void setup() {
   Serial.begin(115200);
-  radio.begin();
-  
+  radio.begin();  
   // Initialize NRF24L01
   if (!radio.begin()) {
     Serial.println("Radio hardware not responding!");
     while (1) {} // Hold in infinite loop
-  }
-  
-  // Set the address
-  radio.openReadingPipe(0, address);
-  //radio.setPALevel(RF24_PA_MIN);  //  
-  // Set module as transmitter
-  radio.stopListening(); 
+    }  
+  radio.openReadingPipe(1, address);
+  radio.setPALevel(RF24_PA_MIN);  //  
+  radio.startListening(); 
   Serial.println("Transmitter initialized");
 }
-
 int i =0;
+float temperature = 0.0;
+int humidity = 0;
 
 void loop() {
-
   delay(2000);
   // Read temperature and humidity using the DHTTempReader class
-  // if (dhtReader.readData()) {
-  //   const float temperature = dhtReader.getTemperature();
-  //   const float humidity = dhtReader.getHumidity();    
-  //   const float temp_f = temperature * 9.0 / 5.0 + 32.0; // Convert to Fahrenheit
-  Serial.print(i);
-  Serial.print("  ");
-  if (radio.available()) {
+  if (radio.available()){
     char text[32] = "";
     radio.read(&text, sizeof(text));
-    Serial.print("Received: ");    
-    Serial.println(text);
+    Serial.print(text);
+    Serial.print(": ");
+
+    // float temperature[32] = 0.0;
+    radio.read(&temperature, sizeof(temperature));
+    Serial.print(temperature);
+    Serial.print("°C, ");
+
+    // float humidity[32] = 0.0;
+    radio.read(&humidity, sizeof(humidity));
+    Serial.print(humidity);
+    Serial.print("%, ");
 
 
-  
+    // Serial.print(i);
+    // Serial.print("  ");
 
     // radio.write(&temperature, sizeof(temperature));
     // Serial.print(temperature);
@@ -63,25 +72,28 @@ void loop() {
     // Serial.print("%, ");
 
       // Serial.println(" %");
-    Serial.println("Data Received !!");  
-    
-  } else {
-    Serial.println("Failed to read Data!");
+    Serial.println("Data received !!");
+  }else{
+    Serial.println("Data did not received !!");
   }
+    i++;
+
+  if (i > 1000)
+  {
+    i=0;
+  }
+}
+
+
   
   // Serial.print(" ");
   // Serial.print(dhtReader.getTemperature());
   // Serial.print(" ");
   // Serial.print(dhtReader.getHumidity());
   // Serial.println(" %");
-  i++;
 
-  if (i > 1000)
-  {
-    i=0;
-  }
 
-}
+
 
 
 
